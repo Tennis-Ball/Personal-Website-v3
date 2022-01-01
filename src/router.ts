974683aebd7e7156f0cc49dda2d3f1b13a9c88b3
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-12-31 23:01:25 -0500

    netlify redirects

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import HomeVue from "./views/Home.vue";
 import ProjectsVue from "./views/Projects.vue";
 import SchoolVue from "./views/School.vue";
 import ServerVue from "./views/Server.vue";
+import Error404 from "./views/404.vue";
 
 const routes = [
   { path: "/", component: HomeVue },
@@ -11,6 +12,7 @@ const routes = [
   { path: "/projects", component: ProjectsVue },
   { path: "/school", component: SchoolVue },
   { path: "/other", component: ServerVue },
+  { path: "/:pathMatch(.*)*", component: Error404 },
 ];
 
 const router = createRouter({
---
+++ src/views/404.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="m-2 text-4xl font-extrabold">404 Not Found</div>
+</template>

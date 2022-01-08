0f63e587f96f7bd4e58a5ca237dd961c6ed10890
junikimm717 <junikimm717@gmail.com>
2022-01-08 15:09:49 -0800

    updated profile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import profile from "../assets/icons/profile.jpg";
+</script>
 <template>
-  <div class="m-2 text-3xl sm:text-4xl font-extrabold">
+  <div class="m-2 text-3xl font-extrabold sm:text-4xl">
     Welcome to Juni Kim's Personal Website!
   </div>
-  <div class="m-2 text-xl sm:text-2xl font-extrabold">About Me</div>
-  <p class="m-2 text-lg">
-    I am a sophomore at Stanford Online High School who partakes in web
-    development, contest math, and linux ricing. Ever since my first programming
-    experience preparing for the USACO, I have explored into other fields in an
-    attempt to find my niche.
-    <br /><br />
-    I am also a nationally ranked competitive fencer as well as a hobbyist of
-    boxing and basketball.
-  </p>
+  <div class="grid grid-cols-1 m-2 sm:grid-cols-4 sm:gap-3">
+    <div class="sm:col-span-3">
+      <div class="text-xl font-extrabold sm:text-2xl">About Me</div>
+      <p class="text-lg">
+        I am a sophomore at Stanford Online High School who partakes in web
+        development, contest math, and linux ricing. Ever since my first
+        programming experience preparing for the USACO, I have explored into
+        other fields in an attempt to find my niche.
+        <br /><br />
+        I am also a nationally ranked competitive fencer as well as a hobbyist
+        of boxing and basketball.
+      </p>
+    </div>
+    <div class="flex justify-center mt-10 sm:col-span-1 sm:m-0">
+      <img v-bind:src="profile" class="mx-auto w-36 rounded-full sm:w-48" />
+    </div>
+  </div>
 </template>

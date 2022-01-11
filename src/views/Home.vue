1af33b960ff9ea7938722a36c24fd85afea9a375
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-01-11 13:15:34 -0500

    Image responsive fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,10 @@ import profile from "../assets/icons/profile.jpg";
   <div class="m-2 text-3xl font-extrabold sm:text-4xl">
     Welcome to Juni Kim's Personal Website!
   </div>
-  <div class="grid grid-cols-1 m-2 sm:grid-cols-4 sm:gap-3">
-    <div class="sm:col-span-3">
+  <div class="m-2 flex flex-row flex-wrap justify-between">
+    <span class="max-w-2xl">
       <div class="text-xl font-extrabold sm:text-2xl">About Me</div>
-      <p class="text-lg">
+      <p class="text-base sm:text-lg">
         I am a sophomore at Stanford Online High School who partakes in web
         development, contest math, and linux ricing. Ever since my first
         programming experience preparing for the USACO, I have explored into
@@ -17,9 +17,12 @@ import profile from "../assets/icons/profile.jpg";
         I am also a nationally ranked competitive fencer as well as a hobbyist
         of boxing and basketball.
       </p>
-    </div>
-    <div class="flex justify-center mt-10 sm:col-span-1 sm:m-0">
-      <img v-bind:src="profile" class="mx-auto w-36 rounded-full sm:w-48" />
+    </span>
+    <div class="flex justify-center mt-10 lg:mt-3 mx-auto">
+      <img
+        v-bind:src="profile"
+        class="mx-auto w-36 h-36 rounded-full sm:w-48 sm:h-48"
+      />
     </div>
   </div>
 </template>

b6b03aca1c2e4d614e90770bc40495df9f26f971
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-01-31 19:19:11 -0500

    More updates

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,97 +1,102 @@
 <script setup lang="ts">
-import chem from "../assets/icons/work/chemistry.svg";
-import code from "../assets/icons/work/code.svg";
-import math from "../assets/icons/work/math.svg";
-import pen from "../assets/icons/work/pen.svg";
-import teach from "../assets/icons/work/teach.svg";
-import vue from "../assets/icons/work/vue.svg";
-import rust from "../assets/icons/work/rust.svg";
-import r from "../assets/icons/work/r.svg";
-import git from "../assets/icons/work/git.svg";
-import db from "../assets/icons/work/db.svg";
-import eye from "../assets/icons/work/eye.svg";
-import volunteer from "../assets/icons/work/volunteer.svg";
-import doc from "../assets/icons/work/document.svg";
+import chem from '../assets/icons/work/chemistry.svg';
+import code from '../assets/icons/work/code.svg';
+import math from '../assets/icons/work/math.svg';
+import pen from '../assets/icons/work/pen.svg';
+import teach from '../assets/icons/work/teach.svg';
+import vue from '../assets/icons/work/vue.svg';
+import rust from '../assets/icons/work/rust.svg';
+import r from '../assets/icons/work/r.svg';
+import git from '../assets/icons/work/git.svg';
+import db from '../assets/icons/work/db.svg';
+import eye from '../assets/icons/work/eye.svg';
+import volunteer from '../assets/icons/work/volunteer.svg';
+import doc from '../assets/icons/work/document.svg';
 
 const icon = {
-  chem,
-  code,
-  math,
-  pen,
-  teach,
-  vue,
-  rust,
-  r,
-  git,
-  db,
-  eye,
-  volunteer,
-  doc,
+	chem,
+	code,
+	math,
+	pen,
+	teach,
+	vue,
+	rust,
+	r,
+	git,
+	db,
+	eye,
+	volunteer,
+	doc,
 };
 
 const props = defineProps<{
-  title: String;
-  icon:
-  | "chem"
-  | "math"
-  | "code"
-  | "teach"
-  | "pen"
-  | "vue"
-  | "rust"
-  | "r"
-  | "git"
-  | "db"
-  | "eye"
-  | "volunteer"
-  | "doc";
-  href?: String;
+	title: String;
+	icon:
+		| 'chem'
+		| 'math'
+		| 'code'
+		| 'teach'
+		| 'pen'
+		| 'vue'
+		| 'rust'
+		| 'r'
+		| 'git'
+		| 'db'
+		| 'eye'
+		| 'volunteer'
+		| 'doc';
+	href?: String;
 }>();
 
 const href = props.href ? (props.href as string) : null;
 </script>
 
 <template>
-  <a
-    v-bind:href="href"
-    v-if="href"
-    class="card bg-gray-200 dark:bg-gray-600 hover:scale-105 group hover:bg-yellow-300 dark:hover:bg-yellow-300 grow"
-  >
-    <div class="flex flex-row m-1 gap-2">
-      <img
-        v-bind:alt="$props.icon"
-        v-bind:src="icon[$props.icon]"
-        class="w-9 h-9 bg-blue-500 p-1 rounded-lg"
-      />
-      <span class="text-xl sm:text-2xl font-bold group-hover:text-orange-500">
-        {{
-          $props.title
-        }}
-      </span>
-    </div>
-    <div class="text-lg group-hover:text-orange-500">
-      <slot></slot>
-    </div>
-  </a>
+	<a
+		v-bind:href="href"
+		v-if="href"
+		class="card bg-gray-200 dark:bg-gray-600 hover:scale-105 group hover:bg-yellow-300 dark:hover:bg-yellow-300 grow"
+	>
+		<div class="flex flex-row m-1 gap-2 justify-between">
+			<div class="flex flex-row gap-2">
+				<img
+					v-bind:alt="$props.icon"
+					v-bind:src="icon[$props.icon]"
+					class="w-9 h-9 bg-blue-500 p-1 rounded-lg"
+				/>
+				<span
+					class="text-xl sm:text-2xl font-bold group-hover:text-orange-500"
+					v-html="$props.title"
+				>
+				</span>
+			</div>
+			<span class="text-sm sm:text-md font-bold text-fuchsia-500">
+				Clickable
+			</span>
+		</div>
+		<div class="text-lg group-hover:text-orange-500">
+			<slot></slot>
+		</div>
+	</a>
 
-  <div class="card bg-gray-200 dark:bg-gray-600 grow" v-else>
-    <div class="flex flex-row m-1 gap-2">
-      <img
-        v-bind:alt="$props.icon"
-        v-bind:src="icon[$props.icon]"
-        class="w-9 h-9 bg-blue-500 p-1 rounded-lg"
-      />
-      <span class="text-xl sm:text-2xl font-bold">{{$props.title}}</span>
-    </div>
-    <div class="text-lg">
-      <slot></slot>
-    </div>
-  </div>
+	<div class="card bg-gray-200 dark:bg-gray-600 grow" v-else>
+		<div class="flex flex-row m-1 gap-2">
+			<img
+				v-bind:alt="$props.icon"
+				v-bind:src="icon[$props.icon]"
+				class="w-9 h-9 bg-blue-500 p-1 rounded-lg"
+			/>
+			<span class="text-xl sm:text-2xl font-bold">{{ $props.title }}</span>
+		</div>
+		<div class="text-lg">
+			<slot></slot>
+		</div>
+	</div>
 </template>
 
 <style scoped>
 .card {
-  @apply m-2 p-3 max-w-lg rounded-2xl
+	@apply m-2 p-3 max-w-lg rounded-2xl
   transition-all ease-linear duration-200;
 }
 </style>
--- src/views/Achievements.vue
+++ src/views/Achievements.vue
@@ -1,95 +1,111 @@
 <script setup lang="ts">
-import Card from "../components/Card.vue";
+import Card from '../components/Card.vue';
+import scholastic from '../assets/certs/Scholastic.pdf';
+import usnco from '../assets/certs/USNCO.pdf';
 </script>
 <template>
-  <div class="text-3xl sm:text-4xl font-extrabold m-2">Achievements</div>
-  <p class="m-2 text-lg">
-    I have competed in numerous olympiads and contests during my high school
-    career.
-  </p>
-  <div class="text-2xl sm:text-3xl font-bold mb-2 mt-10 mx-2">
-    2021-2022 (10th grade)
-  </div>
-  <hr />
-  <div class="flex flex-row flex-wrap">
-    <Card icon="math" title="2022 AIME Qualifier">
-      I qualified for the 2022 AIME (American Invitational Mathematics
-      Examination) by scoring 144/150 on the Fall AMC (American Mathematics
-      Competitions), in which I placed 6th among all 10th graders. I also scored
-      118.5/150 on the AMC 12B.
-    </Card>
-  </div>
+	<div class="text-3xl sm:text-4xl font-extrabold m-2">Achievements</div>
+	<p class="m-2 text-lg">
+		I have competed in numerous olympiads and contests during my high school
+		career.
+	</p>
+	<div class="text-2xl sm:text-3xl font-bold mb-2 mt-10 mx-2">
+		2021-2022 (10th grade)
+	</div>
+	<hr />
+	<div class="cardgrid">
+		<Card icon="math" title="2022 AIME Qualifier">
+			I qualified for the 2022 AIME (American Invitational Mathematics
+			Examination) by scoring 144/150 on the Fall AMC (American Mathematics
+			Competitions), in which I placed 6th among all 10th graders. I also scored
+			118.5/150 on the AMC 12B.
+		</Card>
+		<Card
+			icon="pen"
+			title="2022 Scholastic Writing Regional Gold Key Recipient"
+			v-bind:href="scholastic"
+		>
+			I received a Gold Key from the At-Large East Writing Region for my essay,
+			<i>
+				console.log(“The Web that Won&quot;t Hold”); Software Inefficiency and
+				Electronic Waste
+			</i>
+		</Card>
+	</div>
 
-  <div class="text-2xl sm:text-3xl font-bold mb-2 mt-10 mx-2">
-    2020-21 (9th grade)
-  </div>
-  <hr />
-  <div class="cardgrid">
-    <Card
-      icon="math"
-      title="2021 USAJMO Qualifier"
-      href="https://www.maa.org/sites/default/files/pdf/AMC/usamo/2021/JMO%20List.pdf"
-    >
-      I qualified for the 2021 USAJMO (USA Junior Mathematical Olympiad) with a
-      qualification index of 241. I also scored 7/42 on the USAJMO.
-    </Card>
-
-    <Card
-      icon="code"
-      title="USACO Platinum Competitor"
-      href="http://usaco.org/current/data/jan21_gold_results.html"
-    >
-      I was promoted to the Platinum Division of the USACO (USA Computing
-      Olympiad) in the 2020-21 January Contest by scoring 917/1000.
-    </Card>
-    <Card icon="chem" title="2021 USNCO National Exam Qualifier">
-      I qualified for the 2021 USNCO (USA National Chemistry Olympiad) National
-      Exam through the ACS Silicon Valley Chapter by scoring 51/60 on the local
-      exam.
-    </Card>
-    <Card
-      icon="code"
-      title="2021 ACSL Gold Medalist"
-      href="https://www.scores.acsl.org/legacy/finals/2021/i/"
-    >
-      I received a Gold Medal in the Intermediate Division of the 2021 ACSL
-      (American Computer Science League) Finals by receiving perfect scores in
-      both the short answer and programming sections.
-    </Card>
-    <Card icon="math" title="2021 AIME Qualifier">
-      I qualified for the 2021 AIME by scoring 141/150 on the AMC 10A and
-      117/150 on the AMC 10B. I scored 10/15 on the 2021 AIME I.
-    </Card>
-    <Card
-      icon="code"
-      title="2021 Canadian Computing Competition"
-      href="https://cemc.uwaterloo.ca/contests/past_contests/2021/2021CCCResults.pdf"
-    >
-      I finished in the top 75 out of 2920 competitors in the Senior Division of
-      the 2021 Canadian Computing Competition by scoring 58/75.
-    </Card>
-    <Card icon="pen" title="2021 National Latin Exam">
-      I received a Summa Cum Laude Gold Medal in the 2021 National Latin Exam
-      Beginning Latin Exam.
-    </Card>
-    <Card
-      icon="code"
-      title="2021 CS@Mines HSPC"
-      href="https://mines21.kattis.com/standings"
-    >
-      I organized a team of three students and placed second out of 56
-      competitors in the 2021 Colorado School of Mines High School Programming
-      Competition.
-    </Card>
-  </div>
-  <div class="text-2xl sm:text-3xl font-bold mt-10 mb-2 mx-2">
-    2019-20 (8th grade)
-  </div>
-  <hr />
-  <div class="flex flex-row flex-wrap">
-    <Card title="2020 AIME Qualifier" icon="math">
-      I qualified for the 2020 AIME by scoring 109.5 on the 2020 AMC 10A. I also
-      scored 7/15 on the AIME I.
-    </Card>
-  </div>
+	<div class="text-2xl sm:text-3xl font-bold mb-2 mt-10 mx-2">
+		2020-21 (9th grade)
+	</div>
+	<hr />
+	<div class="cardgrid">
+		<Card
+			icon="math"
+			title="2021 USAJMO Qualifier"
+			href="https://www.maa.org/sites/default/files/pdf/AMC/usamo/2021/JMO%20List.pdf"
+		>
+			I qualified for the 2021 USAJMO (USA Junior Mathematical Olympiad) with a
+			qualification index of 241. I also scored 7/42 on the USAJMO.
+		</Card>
+		<Card
+			icon="code"
+			title="USACO Platinum Competitor"
+			href="http://usaco.org/current/data/jan21_gold_results.html"
+		>
+			I was promoted to the Platinum Division of the USACO (USA Computing
+			Olympiad) in the 2020-21 January Contest by scoring 917/1000.
+		</Card>
+		<Card
+			icon="chem"
+			title="2021 USNCO National Exam Qualifier <i>(Semi-Finalist)</i>"
+			v-bind:href="usnco"
+		>
+			I qualified for the 2021 USNCO (USA National Chemistry Olympiad) National
+			Exam through the ACS Silicon Valley Chapter by scoring 51/60 on the local
+			exam.
+		</Card>
+		<Card
+			icon="code"
+			title="2021 ACSL Gold Medalist"
+			href="https://www.scores.acsl.org/legacy/finals/2021/i/"
+		>
+			I received a Gold Medal in the Intermediate Division of the 2021 ACSL
+			(American Computer Science League) Finals by receiving perfect scores in
+			both the short answer and programming sections.
+		</Card>
+		<Card icon="math" title="2021 AIME Qualifier">
+			I qualified for the 2021 AIME by scoring 141/150 on the AMC 10A and
+			117/150 on the AMC 10B. I scored 10/15 on the 2021 AIME I.
+		</Card>
+		<Card
+			icon="code"
+			title="2021 Canadian Computing Competition"
+			href="https://cemc.uwaterloo.ca/contests/past_contests/2021/2021CCCResults.pdf"
+		>
+			I finished in the top 75 out of 2920 competitors in the Senior Division of
+			the 2021 Canadian Computing Competition by scoring 58/75.
+		</Card>
+		<Card icon="pen" title="2021 National Latin Exam">
+			I received a Summa Cum Laude Gold Medal in the 2021 National Latin Exam
+			Beginning Latin Exam.
+		</Card>
+		<Card
+			icon="code"
+			title="2021 CS@Mines HSPC"
+			href="https://mines21.kattis.com/standings"
+		>
+			I organized a team of three students and placed second out of 56
+			competitors in the 2021 Colorado School of Mines High School Programming
+			Competition.
+		</Card>
+	</div>
+	<div class="text-2xl sm:text-3xl font-bold mt-10 mb-2 mx-2">
+		2019-20 (8th grade)
+	</div>
+	<hr />
+	<div class="flex flex-row flex-wrap">
+		<Card title="2020 AIME Qualifier" icon="math">
+			I qualified for the 2020 AIME by scoring 109.5 on the 2020 AMC 10A. I also
+			scored 7/15 on the AIME I.
+		</Card>
+	</div>
 </template>

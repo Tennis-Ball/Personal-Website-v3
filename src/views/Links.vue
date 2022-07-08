f3eb5c24c853ce88b32d8a378fae8698a5c096b2
Juni Kim <junikimm717@gmail.com>
2022-07-08 12:08:50 -0400

    links reformat

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -4,16 +4,16 @@ import Card from "../components/Card.vue";
 <template>
   <div class="m-2 text-3xl font-extrabold sm:text-4xl">Links</div>
   <div class="cardgrid">
-    <Card href="https://blog.junickim.me" title="My Blog" icon="pen"
-      >I occasionally write about programming and ad-hoc thoughts here.</Card
-    >
+    <Card href="https://blog.junickim.me" title="My Blog" icon="pen">
+      I occasionally write about programming and ad-hoc thoughts here.
+    </Card>
     <Card href="https://git.junickim.me" title="My Git Server" icon="git">
       I usually put minor projects and scripts here so they don't pollute my
       github account.
     </Card>
-    <Card href="/files/Resume.pdf" title="My Resume" icon="doc"
-      >My Full Resume, last updated on July 8, 2022.</Card
-    >
+    <Card href="/files/Resume.pdf" title="My Resume" icon="doc">
+      My Full Resume, last updated on July 8, 2022.
+    </Card>
     <Card
       href="https://nextcloud.junickim.me"
       title="My NextCloud Server"

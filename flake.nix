8312b8895140f4e88027e26ec9f2bb1600205fa7
Juni Kim <junikimm717@gmail.com>
2022-07-20 12:51:32 -0400

    nix flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Personal Website of Juni Kim";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system: 
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShell = pkgs.mkShell {
+      buildInputs = with pkgs; [ yarn ];
+    };
+  });
+}
